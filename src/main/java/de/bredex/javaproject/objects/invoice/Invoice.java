3bdc93551a0c7189ac8da0e271328cc0c5c5310b
tuan <dev@dangschulz.de>
2023-11-04 08:34:41 +0100

    cleanup

--- src/main/java/de/bredex/javaproject/objects/invoice/Invoice.java
+++ src/main/java/de/bredex/javaproject/objects/invoice/Invoice.java
@@ -1,8 +1,6 @@
 package de.bredex.javaproject.objects.invoice;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import de.bredex.javaproject.objects.product.Product;
--- src/main/java/de/bredex/javaproject/objects/user/customer/CustomerController.java
+++ src/main/java/de/bredex/javaproject/objects/user/customer/CustomerController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.GetMapping;
